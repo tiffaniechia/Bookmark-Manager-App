2d2302bc67a565d91283a3ea6649f9ff20afe0c3
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-03-25 22:26:15 +0000

    added password confirmation

--- lib/user.rb
+++ lib/user.rb
@@ -2,13 +2,19 @@ require 'bcrypt' #bcrypt will generate a password hash
 
 class User
 
-    include DataMapper::Resource
-
+  include DataMapper::Resource
+  
     property :id, Serial
     property :email, String
     property :password_digest, Text
 
+    # attr_reader :password
+    attr_accessor :password_confirmation
+
+    validates_confirmation_of :password
+
     def password=(password)
+      @password = password
       self.password_digest = BCrypt::Password.create(password)
     end  
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -7,15 +7,25 @@ feature "User signups" do
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")
   end  
+end 
 
   def sign_up(email = "alice@example.com",
-              password = "oranges!")
+              password = "oranges!",
+              password_confirmation = "oranges!")
     visit '/users/new'
-    # expect(page.status_code).to eq(200)
+    expect(page.status_code).to eq(200)
     expect(page.status_code).to eq(200)
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 
+ 
+feature "password confirmation" do 
+
+  scenario "with a password that doesn't match" do 
+    lambda {sign_up('a@a.com', 'pass', 'wrong')}.should change(User, :count).by(1)
+  end
+    
 end  
