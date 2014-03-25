b1d424f39a80514d9bd430f4d9e0b3b8dcae35b1
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-03-25 23:35:15 +0000

    added email uniqueness checks

--- lib/user.rb
+++ lib/user.rb
@@ -5,15 +5,16 @@ class User
   include DataMapper::Resource
   
     property :id, Serial
-    property :email, String
+    property :email, String, :unique => true
     property :password_digest, Text
 
     attr_reader :password
     attr_accessor :password_confirmation
 
-    validates_confirmation_of :password 
-
+    validates_confirmation_of :password
+    validates_uniqueness_of :email 
     def password=(password)
+
       @password = password
       self.password_digest = BCrypt::Password.create(password)
     end  
